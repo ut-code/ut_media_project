da7a4bb11fe53498d2854f5ade7383cef0258f88
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2022-07-06 17:00:57 +0900

    paper入れた(動作確認はしていない)

--- pages/article.tsx
+++ pages/article.tsx
@@ -3,12 +3,14 @@ import type { NextPage } from 'next'
 import {useState} from 'react'
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
 
 
 class Article{
@@ -29,32 +31,30 @@ const Home: NextPage = () => {
   return (
     <div>
       <Container fixed>
-        <Grid container spacing={2} alignItems="center">
-          <Grid item xs={12}>
-            <FormControlLabel
-              control={
-                <Switch
-                  checked={loginName !== ""}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    if(event.target.checked){
-                      setLoginName("森鴎外")
-                    }else{
-                      setLoginName("");
-                    }
-                  }}
-                />
-              }
-              label="ログイン(テスト用)"
-            />
-            <TextField
-              variant="standard"
-              label="ユーザー名(テスト用)"
-              value={loginName}
+        <FormControlLabel
+          control={
+            <Switch
+              checked={loginName !== ""}
               onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                setLoginName(event.target.value);
+                if(event.target.checked){
+                  setLoginName("森鴎外")
+                }else{
+                  setLoginName("");
+                }
               }}
             />
-          </Grid>
+          }
+          label="ログイン(テスト用)"
+        />
+        <TextField
+          variant="standard"
+          label="ユーザー名(テスト用)"
+          value={loginName}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setLoginName(event.target.value);
+          }}
+        />
+        <Grid container spacing={2} alignItems="center">
           <Grid item xs={12}>
             <Typography variant="h3">
               {article.title}
@@ -79,6 +79,42 @@ const Home: NextPage = () => {
             </Button>
           </Grid>
         </Grid>
+        <Paper sx={{width:'100%'}}>
+          <Typography variant="body1">
+            {article.content}
+          </Typography>
+        </Paper>
+
+        <Typography variant="h4">
+          レビュー
+        </Typography>
+
+        <Stack spacing={2}>
+          <Paper sx={{width}}>
+            <Grid container spacing={2} alignItems="center">
+              <Grid item>
+                <Avatar>
+                  M
+                </Avatar>
+              </Grid>
+              <Grid item xs>
+                <Typography>
+                  森鴎外
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm="auto" justifyContent="flex-end">
+                <Button
+                  variant={"contained"}
+                >
+                  編集
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                とりあえず反対。
+              </Grid>
+            </Grid>
+          </Paper>
+        </Stack>
       </Container>
     </div>
   )
